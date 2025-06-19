536ecb7a29f81cca857a8e851c22d1a5f44abfd2
devloopcode <medidbenouakrim@gmail.com>
2025-06-19 17:58:22 +0100

    cors en

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,8 +3,8 @@ import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { CreateUserDto, LoginDto } from './DTO/login.dto';
 import { JwtAuthGuard } from './JwtAuthGuard';
-import { AuthGuard } from './auth.guard';
-// import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from './auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/v1/auth')
 export class AuthController {
