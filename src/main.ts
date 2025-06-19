829b812fec327ee9df97a6d639c58d097a2e585b
devloopcode <medidbenouakrim@gmail.com>
2025-06-19 17:30:13 +0100

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: process.env.CLIENT_URL!,
+    origin: '*',
     credentials: true,
   });
   await app.listen(PORT, () => Logger.log(`The server is running on port : ${PORT}`));
